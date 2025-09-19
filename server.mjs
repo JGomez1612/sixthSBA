137a726ddb0833e7d278d9b08e00924a64638ddc
JGomez1612 <jostgomez1612@gmail.com>
2025-09-19 19:52:11 -0400

    Initial Commit.

---
+++ server.mjs
@@ -0,0 +1,17 @@
+// Imports
+import express from 'express'
+
+// Setup
+const app = express();
+const PORT = 3000;
+
+// Middleware
+
+// Routes
+
+//Global Error Handling Middleware
+
+// Server Listener
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`)
+});
