d387494f0dbe5db1894ecb3e1ce586054c93d978
JGomez1612 <jostgomez1612@gmail.com>
2025-09-21 17:57:37 -0400

    6th commit. Added game schema.

---
+++ models/gameSchema.mjs
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const gameSchema = new mongoose.Schema({
+    title: { type: String, required: true},
+    developer: {type: String},
+    publisher: {type: String},
+    releaseDate: {type: String},
+    genre: { type: String,
+        enum: {
+            values: [
+                'action',
+                'adventure',
+                'horror',
+                'shooter',
+                'fighting',
+                'platformer',
+                'racing',
+                'rpg',
+                'puzzle',
+            ],
+            message: `{VALUE} is not supported`
+        },
+        required: true},
+});
+
+export default mongoose.model("Game", gameSchema);
